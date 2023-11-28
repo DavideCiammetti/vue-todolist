884aaf1464ba257fd6bd75d970f2d11db55d68d5
DavideCiammetti <davideciammetti97@gmail.com>
2023-11-28 18:31:32 +0100

    feat: milestone 1

--- main.js
+++ main.js
@@ -7,19 +7,22 @@ createApp({
     return {
         list: [
             {
-                'text': 'comprare pasta',
-                'done': true,
-            }, {
-                'text': 'comprare pane',
-                'done': true,
-            }, {
-                'text': 'comprare latte',
-                'done': true,
-            }, {
-                'text': 'comprare uova',
-                'done': true,
+                listItem :[],
+                newItem :'',
+                done : true,
             },
         ],
     };
+  },
+  methods:{
+    // aggiungiamo elementi alla lista 
+    addItems(){
+
+         if(this.list[0].newItem !==  null){
+            this.list[0].listItem.push(this.list[0].newItem);
+            console.log(this.list[0].listItem);
+            this.list[0].newItem = '';
+         }
+    }
   },
 }).mount('#app')
