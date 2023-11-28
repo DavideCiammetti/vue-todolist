e8f33a47e1c8a2ba0ce7a91eca5dc275de05b80f
DavideCiammetti <davideciammetti97@gmail.com>
2023-11-28 16:07:51 +0100

    feat: creazione lista obj

--- main.js
+++ main.js
@@ -5,7 +5,21 @@ const{ createApp } = Vue;
 createApp({
   data() {
     return {
-     
+        list: [
+            {
+                'text': 'comprare pasta',
+                'done': true,
+            }, {
+                'text': 'comprare pane',
+                'done': true,
+            }, {
+                'text': 'comprare latte',
+                'done': true,
+            }, {
+                'text': 'comprare uova',
+                'done': true,
+            },
+        ],
     };
   },
 }).mount('#app')
